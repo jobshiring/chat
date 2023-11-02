6dc225399117ed5dc823cb59e6d89ba1e2a0edde
Parsa Asgari <parsa.asgari3264@gmail.com>
2023-11-02 08:33:29 +0100

    feat_bookmarking-003: Added username arg to root page's Chat component invocation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
+import { auth } from '@/auth'
+import { cookies } from 'next/headers'
 
 export const runtime = 'edge'
 
-export default function IndexPage() {
+export default async function IndexPage() {
   const id = nanoid()
+  const cookieStore = cookies()
+  const session = await auth({ cookieStore })
 
-  return <Chat id={id} />
+  return <Chat id={id} username={session?.user?.email} />
 }
