7ce0b34f78e3e72a0abecd4fdf99792462ce67c5
Parsa Asgari <parsa.asgari3264@gmail.com>
2024-01-11 07:53:28 +0000

    iframe-integration-007: Better separation of logic between searchParams auth and standard auth

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -18,15 +18,7 @@ export default async function SignInPage({
   // legacy code
   // // //  const user_id = searchParams?.user_id
 
-  // if query_params is passed
   const token = searchParams?.token
-  const url = `${process.env.IFRAME_AUTH_API_BASE_URL}/${process.env.IFRAME_AUTH_API_PATH_URL}`
-  const iframe_auth_response = await fetch(url, {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${token}` }
-  })
-  const iframe_auth_response_json = await iframe_auth_response.json()
-  const user_id = iframe_auth_response_json.Success == true ? iframe_auth_response_json?.Family[0]?.DetailsID : undefined
   const BIZGPT_IFRAME_MODE = process.env.BIZGPT_IFRAME_MODE
 
   // redirect to home if user is already logged in
@@ -34,13 +26,30 @@ export default async function SignInPage({
     redirect('/')
   }
 
-  if (user_id && BIZGPT_IFRAME_MODE == 'true') {
+  if (BIZGPT_IFRAME_MODE == 'true' && token) {
+    // if query_params is passed
+
+    if (process.env.DEBUG_MODE) console.log("DEBUG MODE - IFRAME ===> TOKEN: ", token)
+    const url = `${process.env.IFRAME_AUTH_API_BASE_URL}/${process.env.IFRAME_AUTH_API_PATH_URL}`
+    const iframe_auth_response = await fetch(url, {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    const iframe_auth_response_json = await iframe_auth_response.json()
+    if (process.env.DEBUG_MODE) console.log("DEBUG MODE - IFRAME ===> RESPONSE: ", iframe_auth_response_json)
+    const user_id = iframe_auth_response_json.Success == true ? iframe_auth_response_json?.Family[0]?.DetailsID : undefined
     return (
-      <LoginFormSearchParams user_id={user_id} />
+      user_id ? <LoginFormSearchParams user_id={user_id} /> : (
+        <div className="flex h-[calc(100vh-theme(spacing.16))] flex-col items-center justify-center py-10">
+          <div className="w-full max-w-sm">
+            <h1 className="text-rose-500 text-center"> * Invalid Token * </h1>
+            <LoginForm action="sign-in" />
+          </div>
+        </div>)
     )
 
   }
-  
+
 
   return (
     <div className="flex h-[calc(100vh-theme(spacing.16))] flex-col items-center justify-center py-10">
