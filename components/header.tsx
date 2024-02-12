8e3b0d4bf3aae7e6c49466eff92026c14d470b4c
Parsa Asgari <parsa.asgari3264@gmail.com>
2024-02-12 11:16:28 +0000

    small fix for too-many calls to supabase (cause: bizgpt_role)

--- components/header.tsx
+++ components/header.tsx
@@ -22,7 +22,6 @@ export async function Header() {
   const session = await auth({ cookieStore })
   var user_role;
   session?.user ? (user_role = await getUserRole(session?.user?.id)) : (user_role = undefined)
-  // const user_role = await getUserRole(session?.user?.id)
   const BIZGPT_IFRAME_MODE = process.env.BIZGPT_IFRAME_MODE
   return (
     <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
