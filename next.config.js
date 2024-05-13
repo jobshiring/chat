e8fa2f3d4f69437ef0b34b4d554102368bff9435
Ali <amohammadzadeh@interrodata.com>
2024-05-13 09:55:10 +0330

    add Asia Insurance

--- next.config.js
+++ next.config.js
@@ -11,19 +11,30 @@ module.exports = {
     ]
   },
   env: {
-    BizGPT_CLIENT_API_BASE_ADDRESS_SCHEME: process.env.BizGPT_CLIENT_API_BASE_ADDRESS_SCHEME,
+    BizGPT_CLIENT_API_BASE_ADDRESS_SCHEME:
+      process.env.BizGPT_CLIENT_API_BASE_ADDRESS_SCHEME,
     BizGPT_CLIENT_API_BASE_ADDRESS: process.env.BizGPT_CLIENT_API_BASE_ADDRESS,
     BizGPT_CLIENT_API_PORT: process.env.BizGPT_CLIENT_API_PORT,
-    BizGT_CLIENT_API_BOOKMARK_RETRIEVE_PATH: process.env.BizGT_CLIENT_API_BOOKMARK_RETRIEVE_PATH,
-    BizGT_CLIENT_API_BOOKMARK_PERSIST_PATH: process.env.BizGT_CLIENT_API_BOOKMARK_PERSIST_PATH,
-    BizGT_CLIENT_API_FEEDBACK_PERSIST_PATH: process.env.BizGT_CLIENT_API_FEEDBACK_PERSIST_PATH,
-    BizGT_CLIENT_API_FEEDBACK_RETRIEVE_PATH: process.env.BizGT_CLIENT_API_FEEDBACK_RETRIEVE_PATH,
-    BizGT_CLIENT_API_MESSAGES_RETRIEVE_PATH: process.env.BizGT_CLIENT_API_MESSAGES_RETRIEVE_PATH,
-    BizGT_CLIENT_API_MESSAGES_SUBMIT_PATH: process.env.BizGT_CLIENT_API_MESSAGES_SUBMIT_PATH,
-    BizGT_CLIENT_API_ADMIN_EMBEDDING_INSERT_TEXT: process.env.BizGT_CLIENT_API_ADMIN_EMBEDDING_INSERT_TEXT,
-    BizGT_CLIENT_API_ADMIN_RETRIEVE_VECTOR_LOG: process.env.BizGT_CLIENT_API_ADMIN_RETRIEVE_VECTOR_LOG,
-    BizGPT_CLIENT_API_TOKEN_FRONTEND: process.env.BizGPT_CLIENT_API_TOKEN_FRONTEND,
-    BizGT_CLIENT_API_ADMIN_MARKDOWN_INSERT: process.env.BizGT_CLIENT_API_ADMIN_MARKDOWN_INSERT,
+    BizGT_CLIENT_API_BOOKMARK_RETRIEVE_PATH:
+      process.env.BizGT_CLIENT_API_BOOKMARK_RETRIEVE_PATH,
+    BizGT_CLIENT_API_BOOKMARK_PERSIST_PATH:
+      process.env.BizGT_CLIENT_API_BOOKMARK_PERSIST_PATH,
+    BizGT_CLIENT_API_FEEDBACK_PERSIST_PATH:
+      process.env.BizGT_CLIENT_API_FEEDBACK_PERSIST_PATH,
+    BizGT_CLIENT_API_FEEDBACK_RETRIEVE_PATH:
+      process.env.BizGT_CLIENT_API_FEEDBACK_RETRIEVE_PATH,
+    BizGT_CLIENT_API_MESSAGES_RETRIEVE_PATH:
+      process.env.BizGT_CLIENT_API_MESSAGES_RETRIEVE_PATH,
+    BizGT_CLIENT_API_MESSAGES_SUBMIT_PATH:
+      process.env.BizGT_CLIENT_API_MESSAGES_SUBMIT_PATH,
+    BizGT_CLIENT_API_ADMIN_EMBEDDING_INSERT_TEXT:
+      process.env.BizGT_CLIENT_API_ADMIN_EMBEDDING_INSERT_TEXT,
+    BizGT_CLIENT_API_ADMIN_RETRIEVE_VECTOR_LOG:
+      process.env.BizGT_CLIENT_API_ADMIN_RETRIEVE_VECTOR_LOG,
+    BizGPT_CLIENT_API_TOKEN_FRONTEND:
+      process.env.BizGPT_CLIENT_API_TOKEN_FRONTEND,
+    BizGT_CLIENT_API_ADMIN_MARKDOWN_INSERT:
+      process.env.BizGT_CLIENT_API_ADMIN_MARKDOWN_INSERT,
     BIZGPT_ORGANIZATION: process.env.BIZGPT_ORGANIZATION,
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
     PERSISTENCE_MODE: process.env.PERSISTENCE_MODE,
@@ -33,8 +44,9 @@ module.exports = {
     BIZGPT_IFRAME_MODE: process.env.BIZGPT_IFRAME_MODE,
     IFRAME_AUTH_API_BASE_URL: process.env.IFRAME_AUTH_API_BASE_URL,
     IFRAME_AUTH_API_PATH_URL: process.env.IFRAME_AUTH_API_PATH_URL,
-    BIZGPT_FRONTEND_LANGUAGE: process.env.BIZGPT_FRONTEND_LANGUAGE,
-    BizGT_CLIENT_API_FEEDBACK_ADMIN_RETRIEVE_PATH: process.env.BizGT_CLIENT_API_FEEDBACK_ADMIN_RETRIEVE_PATH,
+    BIZGPT_FRONTEND_LANGUAGE: process.env.NEXT_PUBLIC_BIZGPT_FRONTEND_LANGUAGE,
+    BizGT_CLIENT_API_FEEDBACK_ADMIN_RETRIEVE_PATH:
+      process.env.BizGT_CLIENT_API_FEEDBACK_ADMIN_RETRIEVE_PATH
   },
   compiler: {
     // Enables the styled-components SWC transform
@@ -44,12 +56,19 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/:path*",
+        source: '/:path*',
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
         ]
       }
     ]
